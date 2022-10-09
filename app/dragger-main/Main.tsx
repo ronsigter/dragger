d3eacce4500e304bb1115a13c34d936afd4ebb60
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-09 15:22:57 +0800

    adds sign out button

--- app/dragger-main/Main.tsx
+++ app/dragger-main/Main.tsx
@@ -1,25 +1,44 @@
-import { Box, Grid, GridItem } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Heading,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+import Router from 'next/router'
+import { Routes } from 'routes'
 import { GridBox } from './components'
 
 export const Main: React.FC = () => {
   const rows = Array.from({ length: 10 })
   const columns = Array.from({ length: 10 })
 
+  const handleSignOut = (): void => {
+    Router.push(Routes.LOGIN)
+  }
+
   return (
-    <Box border='solid 1px black'>
-      {rows.map((_arr, r) => (
-        <Grid key={r} templateColumns='repeat(10, 1fr)'>
-          {columns.map((_arr, c) => (
-            <GridItem
-              key={c}
-              role='presentation'
-              aria-label={`${r + 1}-${c + 1}`}
-            >
-              <GridBox row={r + 1} column={c + 1} />
-            </GridItem>
-          ))}
-        </Grid>
-      ))}
+    <Box>
+      <Box border='solid 1px black'>
+        {rows.map((_arr, r) => (
+          <Grid key={r} templateColumns='repeat(10, 1fr)'>
+            {columns.map((_arr, c) => (
+              <GridItem
+                key={c}
+                role='presentation'
+                aria-label={`${r + 1}-${c + 1}`}
+              >
+                <GridBox row={r + 1} column={c + 1} />
+              </GridItem>
+            ))}
+          </Grid>
+        ))}
+      </Box>
+      <Box textAlign='center' py='10'>
+        <Button onClick={handleSignOut}>Sign out</Button>
+      </Box>
     </Box>
   )
 }
